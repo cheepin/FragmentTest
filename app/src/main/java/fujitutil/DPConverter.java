cbbcee6738a6586fdc6bfae1dcd7ce585fd02b97
windmill mc <fujitsubo1324@gmail.com>
2018-05-19 13:33:29 +0900

    start git

---
+++ app/src/main/java/fujitutil/DPConverter.java
@@ -0,0 +1,12 @@
+package fujitutil;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+
+public class DPConverter
+{
+    public static int convertPx2Dp(int px, Context context){
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return (int) (px / metrics.density);
+    }
+}
